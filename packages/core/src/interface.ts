651ae559551f9603e710bd727f4d705a752daa53
onokumus <onokumus@gmail.com>
2018-09-27 12:22:19 +0300

    add comments, add types

--- packages/core/src/interface.ts
+++ packages/core/src/interface.ts
@@ -1,4 +1,23 @@
-export interface IOCDefault {
+export interface IOcOption {
+  /**
+   * Hides the onoffcanvas when click outside the onoffcanvas
+   *
+   * @type {boolean}
+   * @default true
+   * @memberof IOCDefault
+   */
   createDrawer?: boolean;
+
+  /**
+   * Hides the onoffcanvas when escape key is pressed.
+   *
+   * @type {boolean}
+   * @default true
+   * @memberof IOCDefault
+   */
   hideByEsc?: boolean;
 }
+
+export type OcAction = "toggle" | "show" | "hide";
+
+export type OcEvent = "show.onoffcanvas" | "hide.onoffcanvas";
